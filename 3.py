56a227905ed823c62847ae5902f064d7e323551c
Bartosz Bryniarski <brylka@jelenia.com>
2025-11-29 11:24:02 +0100

    Sobota

---
+++ 3.py
@@ -0,0 +1,9 @@
+# for a in range(10,-11,-5):
+#     print(f"{a} Witaj świecie!")
+# print("Pętla zakończona!")
+
+for b in range(1,11):
+    for a in range(1,11):
+        print(a*b, end="\t")
+    print()
+
