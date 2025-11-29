015ae8c0dce4b445ca203ef4db87428a2442974e
Bartosz Bryniarski <brylka@jelenia.com>
2025-11-29 11:36:31 +0100

    Sobota

--- 3.py
+++ 3.py
@@ -2,8 +2,17 @@
 #     print(f"{a} Witaj świecie!")
 # print("Pętla zakończona!")
 
-for b in range(1,11):
-    for a in range(1,11):
-        print(a*b, end="\t")
+for b in range(0,11):
+    for a in range(0,11):
+        if a == 0 and b == 0:
+            print("\033[31m*\033[0m", end="\t")
+        elif a == 0:
+            print(b, end="\t")
+        elif b == 0:
+            print(a, end="\t")
+        elif a == b:
+            print('\033[31m', a * b, '\033[0m', sep="", end="\t")
+        else:
+            print(a*b, end="\t")
     print()
 
