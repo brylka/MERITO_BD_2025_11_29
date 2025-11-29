557bd1f6351af6e8ba3b15d9221028e4ea90e877
Bartosz Bryniarski <brylka@jelenia.com>
2025-11-29 12:50:56 +0100

    Sobota

--- 4.py
+++ 4.py
@@ -1,5 +1,14 @@
-lista = [5,3,6,[1,[3.15,'Bartosz',13],3],10,-5,4,2,4,6,4,3,56,7,0,9,7,43,534,23,32,32,4]
-#        0 1 2 3       4
-print(lista[3][1][0])
+# lista = [5,3,6,[1,[3.15,'Bartosz',13],3],10,-5,4,2,4,6,4,3,56,7,0,9,7,43,534,23,32,32,4]
+# #        0 1 2 3       4
+# print(lista[3][1][0])
 # print(len(lista))
 # print(lista[-23])
+
+imiona = []
+
+print("Podaj pięć imion:")
+for _ in range(5):
+    imie = input("Podaj imię: ")
+    imiona.append(imie)
+
+print(imiona)
