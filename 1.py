93c7d8c0fab944477f014e0173dc2fce8d152842
Bartosz Bryniarski <brylka@jelenia.com>
2025-11-29 09:38:07 +0100

    Initial commit

---
+++ 1.py
@@ -0,0 +1,16 @@
+# name = "Bartosz"
+# last_name = "Bryniarski"
+# age = 18.5234234
+#
+# print(name, end=" ")
+# print(last_name, end=" ")
+# print(age)
+#
+# print(name + " " + last_name + " " + str(int(age)))
+# print(name, last_name, int(age), sep=" ")
+# print(f"{name} {last_name} {age:.4f}")
+# print("name, last_name, age")
+
+a = 10 == 9
+
+print(a)
