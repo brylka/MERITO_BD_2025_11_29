11dbeaa86043895fa008cf06934e9105d3146de5
Bartosz Bryniarski <brylka@jelenia.com>
2025-11-29 10:31:41 +0100

    Sobota

--- 2.py
+++ 2.py
@@ -1,16 +1,31 @@
-print("+"*50,"Moja aplikacja","+"*50, sep="\n")
+# print("+"*50,"Moja aplikacja","+"*50, sep="\n")
+#
+# a = float(input("Podaj liczbę pierwszą: "))
+# b = float(input("Podaj liczbę drugą: "))
+# c = a + b
+#
+# print(f"Wynik dodawania: {a} + {b} = {c}")
+# print(f"Wynik odejmowania: {a} - {b} = {a-b}")
+# print(f"Wynik mnożenia: {a} * {b} = {a*b}")
+# if b == 0:
+#     print("Nie dzieli się przez zero")
+# else:
+#     print(f"Wynik dzielenia: {a} : {b} = {a/b}")
+#     print(f"Część całkowita dzielenia: {a//b}")
+#     print(f"Reszta z dzielenia: {a%b}")
 
-a = float(input("Podaj liczbę pierwszą: "))
-b = float(input("Podaj liczbę drugą: "))
-c = a + b
-
-print(f"Wynik dodawania: {a} + {b} = {c}")
-print(f"Wynik odejmowania: {a} - {b} = {a-b}")
-print(f"Wynik mnożenia: {a} * {b} = {a*b}")
-if b == 0:
-    print("Nie dzieli się przez zero")
+a = 4
+b = 4
+if a == b:
+    print("równe")
+elif a > b:
+    print("a jest wieksze od b")
+elif a < b:
+    print("a jest mniejsze od b")
+elif a != b:
+    print("nie równe")
 else:
-    print(f"Wynik dzielenia: {a} : {b} = {a/b}")
-    print(f"Część całkowita dzielenia: {a//b}")
-    print(f"Reszta z dzielenia: {a%b}")
+    print("to nie zostanie nigdy wykonane")
 
+# >=
+# <=
