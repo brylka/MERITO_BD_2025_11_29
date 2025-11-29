604312feedad4fdb9e888599d1ad38203691b708
Bartosz Bryniarski <brylka@jelenia.com>
2025-11-29 10:08:18 +0100

    Sobota

--- 2.py
+++ 2.py
@@ -1,5 +1,13 @@
-a = 19
-b = "48"
-
 print("+"*50,"Moja aplikacja","+"*50, sep="\n")
+
+a = float(input("Podaj liczbę pierwszą: "))
+b = float(input("Podaj liczbę drugą: "))
+c = a + b
+
+print(f"Wynik dodawania: {a} + {b} = {c}")
+print(f"Wynik odejmowania: {a} - {b} = {a-b}")
+print(f"Wynik mnożenia: {a} * {b} = {a*b}")
+print(f"Wynik dzielenia: {a} : {b} = {a/b}")
+print(f"Część całkowita dzielenia: {a//b}")
+print(f"Reszta z dzielenia: {a%b}")
 
