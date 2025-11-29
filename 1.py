65bffb7163e3ffeebe403d78006ed3003ba6d48b
Bartosz Bryniarski <brylka@jelenia.com>
2025-11-29 09:39:59 +0100

    Sobota

--- 1.py
+++ 1.py
@@ -1,16 +1,17 @@
-# name = "Bartosz"
-# last_name = "Bryniarski"
-# age = 18.5234234
-#
-# print(name, end=" ")
-# print(last_name, end=" ")
-# print(age)
-#
-# print(name + " " + last_name + " " + str(int(age)))
-# print(name, last_name, int(age), sep=" ")
-# print(f"{name} {last_name} {age:.4f}")
-# print("name, last_name, age")
+name = "Bartosz"
+last_name = "Bryniarski"
+age = 18.5234234
 
-a = 10 == 9
+print(name, end=" ")
+print(last_name, end=" ")
+print(age)
+
+print(name + " " + last_name + " " + str(int(age)))
+print(name, last_name, int(age), sep=" ")
+print(f"{name} {last_name} {age:.4f}")
+print("name, last_name, age")
+
+a = 10 == 10
 
 print(a)
+print("MERITO")
