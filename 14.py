1917aac0952bd59e18f965a1a1ac4ffcc47efbde
Bartosz Bryniarski <brylka@jelenia.com>
2025-11-30 12:30:51 +0100

    Niedziela

---
+++ 14.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+print(datetime.now().timestamp())
+print(datetime.now().year)
+print(datetime.now().month)
+print(datetime.now().day)
+print(datetime.now().hour)
+print(datetime.now().minute)
+print(datetime.now().second)
+print(datetime.now())
+print(datetime.now().strftime("%Y.%m.%d %H:%M:%S"))
